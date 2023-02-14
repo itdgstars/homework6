765a0d61bfee0a2c4f8c06b5aaccc0bca83e22be
SergeyK <it-dgstars@mail.ru>
2023-02-14 22:44:30 +0300

    Решили задачу 41

---
+++ z41/Program.cs
@@ -0,0 +1,19 @@
+﻿void numb()
+{
+Console.WriteLine("Начните вводить числа. Если вы хотите закончить ввод, напишитe число '999'");
+int sum = 0;
+int B = 0;
+while (B != 999)
+{
+   Console.WriteLine ("Введите число: ");
+ 
+B = Convert.ToInt32(Console.ReadLine());
+  if (B > 0)
+  {
+    sum++;
+  } 
+}
+Console.WriteLine ("Количество чисел больше 0 ранво: " + sum);
+}
+
+numb();
